d43a7b82c17811c5369554cf046c0b8e883052a7
Holz-i <132549073+Holz-i@users.noreply.github.com>
2023-05-04 11:38:09 +0200

    Add files via upload

---
+++ lab_1_3.py
@@ -0,0 +1,18 @@
+c=input("Geben Sie ihren Namen an: ") #Nur String eingabe (erklärung für sitznachbar) wert auf c überschreiben
+
+a=int(input("Zahl 1: ")) #Eingabeauforderung und den Input (string) auf int definieren wert auf a überschreiben    
+
+b=int(input("Zahl 2: ")) #Eingabeauforderung und den Input (string) auf int definieren wert auf b überschreiben  
+
+print("Herzlich Wilkommen: "+ c) #Ausgabe auf den Monitor mit dem vorher eingetragenen Namen
+
+
+while b==0: #while ist eine Schleife und wird durch einzug gestartet und beendet    
+    b=int(input("Zahl 2: ")) #Eingabeauforderung und den Input (string) auf int defi
+            
+print(a+b)  #plus mit Ausgabe
+print(a-b)  #minus mit Ausgabe
+print(a/b)  #durch mit Ausgabe
+print(a*b)  #mal mit Ausgabe
+    
+print("Danke für die Nutzung meines Programmes, ich wünsche einen wunderschönen Tag!")
