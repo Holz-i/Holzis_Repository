1f97721351f2cac691d12474451a39c4c66e3205
Holz-i <132549073+Holz-i@users.noreply.github.com>
2023-05-04 10:31:10 +0200

    Add files via upload

---
+++ Lab_1_1.py
@@ -0,0 +1,12 @@
+c=input("Geben Sie ihren Namen an: ") #Nur String eingabe (erklärung für sitznachbar) wert auf c überschreiben
+
+a=int(input("Zahl 1: ")) #Eingabeauforderung und den Input (string) auf int definieren wert auf a überschreiben    
+
+b=int(input("Zahl 2: ")) #Eingabeauforderung und den Input (string) auf int definieren wert auf b überschreiben  
+
+print("Herzlich Wilkommen: "+ c) #Ausgabe auf den Monitor mit dem vorher eingetragenen Namen
+
+print(a+b)  #plus
+print(a-b)  #minus
+print(a/b)  #durch
+print(a*b)  #mal
